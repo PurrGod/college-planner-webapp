62d92e6bfe8ee57e551c806d64dd1bb6a8de6d78
PurrGod <srinid369@gmail.com>
2024-01-20 17:49:55 -0800

    Search bar stuff

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,54 @@
 // Search.js
 
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select";
 import "./Search.css";
 
 const Search = () => {
+  const initialMajors = [
+    "Computer Science",
+    "Biology",
+    "Psychology",
+    "Environmental Studies",
+    "Economics",
+    "Mathematics",
+    "History",
+    "Art",
+    "Physics",
+    "Sociology",
+  ];
+
+  const formatOptions = (majors) => {
+    return majors.map((major) => ({
+      label: major,
+      value: major,
+    }));
+  };
+
+  const [majors, setMajors] = useState(formatOptions(initialMajors));
+  const [selectedMajor, setSelectedMajor] = useState(null);
+
+  const handleSearch = (selectedOption) => {
+    setSelectedMajor(selectedOption);
+  };
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      width: "100%", // Set width to 100%
+    }),
+  };
+
   return (
-    <div className="search-bar">
-      <input type="text" placeholder="Search for UCSC Majors..." />
+    <div  className="search-bar">
+      <Select style={{width: '70%'}}
+        placeholder="Search for UCSC Majors..."
+        options={majors}
+        isSearchable
+        value={selectedMajor}
+        onChange={handleSearch}
+        styles={customStyles} // Apply custom styles
+      />
       <button>Search</button>
     </div>
   );
