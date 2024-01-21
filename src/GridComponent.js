683882b083ce59689870761f303f8e994bc686ab
Amrita Pasupuleti <amritapasupuleti@gmail.com>
2024-01-20 17:56:05 -0800

    added columns titles for schedule

--- src/GridComponent.js
+++ src/GridComponent.js
@@ -6,110 +6,87 @@ import React, { useState } from "react";
 const initialClasses = ["Class A", "Class B", "Class C"];
 
 function GridComponent() {
-    const [gridData, setGridData] = useState([
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-    ]);
+  const quarters = ["Fall", "Winter", "Spring", "Summer"];
 
-    const handleDragStart = (e, rowIndex, colIndex) => {
-        const text = gridData[rowIndex][colIndex];
-        // Remove item from gridData
-        const updatedGrid = [...gridData];
-        updatedGrid[rowIndex].splice(colIndex, 1);
-        setGridData(updatedGrid);
+  const [gridData, setGridData] = useState([
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ]);
 
-    e.dataTransfer.setData("text", text);
-    e.dataTransfer.setData("sourceGrid", "currentGrid");
-    e.dataTransfer.setData("sourceRowIndex", rowIndex.toString());
-    e.dataTransfer.setData("sourceColIndex", colIndex.toString());
+  const handleDragStart = (e, rowIndex, cellIndex) => {
+    e.dataTransfer.setData("text", gridData[rowIndex][cellIndex]);
+    e.dataTransfer.setData("sourceGrid", "grid");
   };
 
-  const handleDragOver = (e, rowIndex, colIndex) => {
+  const handleDragOver = (e, rowIndex) => {
     e.preventDefault();
-    const text = e.dataTransfer.getData("text");
-    e.dataTransfer.setData("text", text);
   };
 
   const handleDrop = (e, rowIndex) => {
     e.preventDefault();
-    const text = e.dataTransfer.getData("text");
+
+    const draggedText = e.dataTransfer.getData("text");
     const sourceGrid = e.dataTransfer.getData("sourceGrid");
-    const sourceRowIndex = parseInt(
-      e.dataTransfer.getData("sourceRowIndex"),
-      10
-    );
-    const sourceColIndex = parseInt(
-      e.dataTransfer.getData("sourceColIndex"),
-      10
-    );
 
-    if (sourceGrid === "currentGrid") {
-      // Handle drop from the same grid
-      const updatedGrid = [...gridData];
-      // Re-insert the dragged text into the target grid cell
-      updatedGrid[rowIndex].push(text);
-
-      // If it's a move within the same row, handle removal from the source cell
-      if (rowIndex === sourceRowIndex) {
-        updatedGrid[sourceRowIndex].splice(sourceColIndex, 1);
-      }
-      setGridData(updatedGrid);
-    } else {
-      // Handle drop from classes-list
-      const updatedGrid = [...gridData];
-      updatedGrid[rowIndex].push(text);
-      setGridData(updatedGrid);
+    if (sourceGrid === "grid") {
+      // Handle drop within the grid
+      const updatedGridData = [...gridData];
+      updatedGridData[rowIndex] = updatedGridData[rowIndex].map((text, j) =>
+        j === rowIndex ? draggedText : text
+      );
+      setGridData(updatedGridData);
+    } else if (sourceGrid === "classes-list") {
+      // Handle drop from classes list to grid
+      const updatedGridData = [...gridData];
+      updatedGridData[rowIndex].push(draggedText);
+      setGridData(updatedGridData);
     }
   };
 
   return (
-    <div className="grid-container">
-      {gridData.map((row, i) => (
-        <div
-          key={i}
-          onDragOver={(e) => handleDragOver(e, i)}
-          onDrop={(e) => handleDrop(e, i)}
-          className="grid-cell"
-        >
-          {row.map((text, j) => (
-            <div
-              key={text}
-              onDragStart={(e) => handleDragStart(e, i, j)}
-              draggable
-            >
-              {text}
-            </div>
-          ))}
-        </div>
-      ))}
-
-      {initialClasses.map((className) => (
-        <div
-          key={className}
-          draggable
-          onDragStart={(e) => {
-            e.dataTransfer.setData("text", className);
-            e.dataTransfer.setData("sourceGrid", "classes-list");
-          }}
-        >
-          {className}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="column-title-container">
+        {quarters.map((quarter, index) => (
+          <React.Fragment key={index}>
+            <div className="quarter-bar-item">{quarter}</div>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="grid-container">
+        {gridData.map((row, i) => (
+          <div
+            key={i}
+            onDragOver={(e) => handleDragOver(e, i)}
+            onDrop={(e) => handleDrop(e, i)}
+            className="grid-cell"
+          >
+            {row.map((text, j) => (
+              <div
+                key={text}
+                onDragStart={(e) => handleDragStart(e, i, j)}
+                draggable
+              >
+                {text}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
