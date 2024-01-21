6c8716792f32ed82944443c0519246baf9f0c9fc
Amrita Pasupuleti <amritapasupuleti@gmail.com>
2024-01-20 21:29:07 -0800

    updated grid component

--- src/GridComponent.js
+++ src/GridComponent.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 
 function GridComponent() {
   const quarters = ["Fall", "Winter", "Spring", "Summer"];
+  const years = ["1st year", "2nd year", "3rd year", "4th year"];
 
   const [gridData, setGridData] = useState([
     [],
@@ -55,35 +56,48 @@ function GridComponent() {
   };
 
   return (
-    <>
-      <div className="column-title-container">
-        {quarters.map((quarter, index) => (
-          <React.Fragment key={index}>
-            <div className="quarter-bar-item">{quarter}</div>
-          </React.Fragment>
-        ))}
+    <div className="schedule-wrapper">
+      <div className="section1">
+        <div className="year-bar">
+          {years.map((year, index) => (
+            <div key={index} className="year-label">
+              {year}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="grid-container">
-        {gridData.map((row, i) => (
-          <div
-            key={i}
-            onDragOver={(e) => handleDragOver(e, i)}
-            onDrop={(e) => handleDrop(e, i)}
-            className="grid-cell"
-          >
-            {row.map((text, j) => (
-              <div
-                key={text}
-                onDragStart={(e) => handleDragStart(e, i, j)}
-                draggable
-              >
-                {text}
-              </div>
-            ))}
-          </div>
-        ))}
+
+      <div className="section2">
+        <div className="column-title-container">
+          {quarters.map((quarter, index) => (
+            <React.Fragment key={index}>
+              <div className="quarter-bar-item">{quarter}</div>
+            </React.Fragment>
+          ))}
+        </div>
+
+        <div className="grid-container">
+          {gridData.map((row, i) => (
+            <div
+              key={i}
+              onDragOver={(e) => handleDragOver(e, i)}
+              onDrop={(e) => handleDrop(e, i)}
+              className="grid-cell"
+            >
+              {row.map((text, j) => (
+                <div
+                  key={text}
+                  onDragStart={(e) => handleDragStart(e, i, j)}
+                  draggable
+                >
+                  {text}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
